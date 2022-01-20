c2a16fffd3a4eba6204184f778709563decf833c
ShoReagan <shoreaganw@gmail.com>
2022-01-20 09:37:30 -0600

    renaming

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class hello_bonus{
+public class Hello{
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Enter Name");
--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class hello_extreme_bonus{
+public class Hello{
     public static void main(String[] args){
 
         String command = "git config user.name";
