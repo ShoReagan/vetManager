5ceacf7800951d8843b299845c740f02f410dbe2
ShoReagan <shoreaganw@gmail.com>
2022-01-28 14:10:47 -0600

    finished bonus, tweaked full_credit

--- P02/bonus/Polygon.java
+++ P02/bonus/Polygon.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Polygon {  
     private int sides;
     private double perimeter;
+    private double[] lengths = new double[255];
 
     public Polygon(int sides, double perimeter) {
         this.sides = sides;
@@ -11,8 +12,8 @@ public class Polygon {
 
     double addSide(double length) {
         if(length > 0) {
-            sides++;
-            perimeter += length;
+            lengths[sides] = length;
+            sides++;    
         }
         return length;
     }
@@ -22,19 +23,30 @@ public class Polygon {
     }
 
     double getPerimeter() {
+        for(double i : lengths) {
+            perimeter += i;
+        }
         return perimeter;
+    }
+
+    double getArea(double apothem) {
+        return perimeter * .5 * apothem;
     }
 
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         Polygon polygon = new Polygon(0, 0);
         double length = 0;
+        double apothem = 0;
 
         System.out.print("Side Length (0 when done): ");
         while(polygon.addSide(myScanner.nextDouble()) > 0) {
             System.out.print("Side Length (0 when done): ");
 
         }
+        System.out.print("Apothem: ");
+        apothem = myScanner.nextDouble();
         System.out.printf("Perimeter of %d-sided polygon is %.2f\n", polygon.getSides(), polygon.getPerimeter());
+        System.out.printf("Area is %.2f\n", polygon.getArea(apothem));
     }
 }
