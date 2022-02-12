64bd0e6edb0c99e0c174f5ecc54ffb1fc22dcb11
ShoReagan <shoreaganw@gmail.com>
2022-02-12 12:51:25 -0600

    almost done, decrpty not working

---
+++ P04/full_credit/Crypto.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Crypto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Scanner strscanner = new Scanner(System.in);
+        String key = new String(args[0]);
+
+        while(true){   
+            try{
+                Substitution sub = new Substitution(key);
+
+                while(true) {
+                    System.out.println("(e)crypt, (d)ecrypt, or (q)uit? ");
+                    char choice = scanner.next().charAt(0);
+
+                    if(choice == 'e'){
+                        System.out.println("Enter text to encrypt");
+                        String txt = new String(strscanner.nextLine());
+                        System.out.println(sub.encrypt(txt));
+                    }
+                    else if(choice == 'd'){
+                        System.out.println("Enter text to decrypt");
+                        String txt = new String(strscanner.nextLine());
+                        System.out.println(sub.decrypt(sub.encrypt(txt)));
+                    }
+                    else if(choice == 'q')
+                        System.exit(1);
+                    else
+                        System.out.println("Not a valid input");
+                    
+            
+                }
+            }
+            catch(IllegalArgumentException e) {
+                System.out.println(e);
+                System.out.print("Enter a valid key: ");
+                key = scanner.nextLine();
+            }
+        }
+    }
+                
+
+
+}
--- P04/full_credit/Substitution.java
+++ P04/full_credit/Substitution.java
@@ -1,25 +1,51 @@
 public class Substitution extends Cypher {
-    private char encryptKey[26];
-    private char decryptKey[26];
+    private char[] encryptKey = new char[26];
+    private char[] decryptKey = new char[26];
 
     public Substitution(String key) {
         if(key.length() != 26)
             throw new IllegalArgumentException("The key is not exactly 26 characters");    
         for(int i = 0; i < 26; i++) {
-            for(int j = 0; j < 26; j++) {
+            for(int j = i + 1; j < 26; j++) {
                 if(key.charAt(i) == key.charAt(j))
                     throw new IllegalArgumentException("The key does not have all unique characters");
-                }
             }
+        }
+        for(int k = 0; k < 26; k++) {
+            this.encryptKey[k] = key.charAt(k);
+        }
+        this.decryptKey = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     }
 
     public String encrypt(String unencrypted) {
+        StringBuilder ans = new StringBuilder(unencrypted);
 
+        for(int i = 0; i < unencrypted.length(); i++) {
+            if(Character.isLetter(unencrypted.charAt(i))) {
+                if(Character.isUpperCase(unencrypted.charAt(i))) {
+                    ans.setCharAt(i, Character.toUpperCase(encryptKey[unencrypted.charAt(i) - 'A']));
+                }
+                else
+                    ans.setCharAt(i, encryptKey[unencrypted.charAt(i) - 'a']);
+            }
+        }
 
+        return ans.toString();
     }
 
     public String decrypt(String encrypted) {
+        StringBuilder ans = new StringBuilder(encrypted);
 
+        for(int i = 0; i < encrypted.length(); i++) {
+            if(Character.isLetter(encrypted.charAt(i))) {
+                if(Character.isUpperCase(encrypted.charAt(i))) {
+                    ans.setCharAt(i, Character.toUpperCase(decryptKey[encrypted.charAt(i) - 'A']));
+                }
+                else
+                    ans.setCharAt(i, decryptKey[encrypted.charAt(i) - 'a']);
+            }
+        }
 
+        return ans.toString();
     }
 }
