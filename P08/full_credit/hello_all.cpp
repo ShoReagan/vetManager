f5bdb5f337c8abcab1d4878aa90b560963bebb0b
ShoReagan <shoreaganw@gmail.com>
2022-03-22 14:57:37 -0500

    finished full credit

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "What's your name? ";
+    std::getline(std::cin, name);
+    std::cout << "Hello, " << name << "\n";
+}
